26869b26a5722868f33d138861164d17b4130d7c
shivam anand <shivam850anand@gmail.com>
2025-05-10 12:25:57 +0530

    header-responsive added to codebase

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -6,7 +6,7 @@ import { Badge } from "../ui/badge";
 
 const Header = () => {
   return (
-    <div className="sticky top-0 left-0 z-50 mx-auto mt-2 flex w-[85%] rounded-xl bg-zinc-200 text-primary dark:bg-zinc-900">
+    <div className="text-primary sticky top-0 left-0 z-50 mx-auto flex w-full bg-zinc-200 lg:mt-2 lg:w-[85%] lg:rounded-xl dark:bg-zinc-900">
       <nav className="mx-auto flex flex-1 items-center justify-between rounded-b-3xl px-4 py-3">
         <div className="center gap-4">
           <span className="bg-primary text-card border-primary rounded-xl border p-2">
@@ -17,30 +17,32 @@ const Header = () => {
             <p className="text-xs">UI component library</p>
           </span>
         </div>
-        <div className="flex items-center gap-x-2">
-          <Link
-            className="border-primary flex items-center gap-x-2 rounded-xl border px-2 py-1 text-sm"
-            href={"/compoents"}
-          >
-            Compoents
-          </Link>
-          <Link
-            className="border-primary flex items-center gap-x-2 rounded-xl border px-2 py-1 text-sm"
-            href={"/compoents"}
-          >
-            Templates
-            <span className="flex items-center gap-x-1">
-              <Badge variant={"secondary"}>
-                <Tag size={18} /> new
-              </Badge>
-            </span>
-          </Link>
-          <Link
-            className="border-primary flex items-center gap-x-2 rounded-xl border px-2 py-1 text-sm"
-            href={"/compoents"}
-          >
-            Pricing
-          </Link>
+        <div className="mobile">
+          <div className="flex items-center gap-x-2">
+            <Link
+              className="border-primary flex items-center gap-x-2 rounded-xl border px-2 py-1 text-sm"
+              href={"/compoents"}
+            >
+              Compoents
+            </Link>
+            <Link
+              className="border-primary flex items-center gap-x-2 rounded-xl border px-2 py-1 text-sm"
+              href={"/compoents"}
+            >
+              Templates
+              <span className="flex items-center gap-x-1">
+                <Badge variant={"secondary"}>
+                  <Tag size={18} /> new
+                </Badge>
+              </span>
+            </Link>
+            <Link
+              className="border-primary flex items-center gap-x-2 rounded-xl border px-2 py-1 text-sm"
+              href={"/compoents"}
+            >
+              Pricing
+            </Link>
+          </div>
         </div>
         <div className="flex items-center justify-between gap-x-2">
           {/* THEME TOGGLE */}
--- src/components/shared/sticky-banner.tsx
+++ src/components/shared/sticky-banner.tsx
@@ -2,12 +2,15 @@ import { StickyBanner } from "../ui/sticky-banner";
 
 export default function Stickybanner() {
   return (
-    <div className="relative flex w-full text-sm flex-col overflow-y-auto">
+    <div className="relative flex w-full flex-col overflow-y-auto text-sm">
       <StickyBanner className="bg-sidebar-primary">
         <span className="text-lg">🚀</span>
-        <p className="ml-3 max-w-[90%] text-white drop-shadow-md">
+        <p className="ml-3 max-w-[90%] text-white drop-shadow-md @md:text-justify">
           New components coming soon to our UI library! Stay tuned for updates.
-          <a href="#" className="transition border border-blue-500 bg-yellow-100 ml-3 text-black p-1 rounded-sm duration-200 hover:underline">
+          <a
+            href="#"
+            className="ml-3 rounded-sm border border-blue-500 bg-yellow-300 p-1 text-black transition duration-200 hover:underline"
+          >
             Read announcement
           </a>
         </p>

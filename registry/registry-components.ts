2f635cab2ff5761359ea62a750b2e3c52b8f478e
Shivam03-coder <shivam850anand@gmail.com>
2025-05-22 18:05:08 +0530

    dialog added to codebase

--- registry/registry-components.ts
+++ registry/registry-components.ts
@@ -61,5 +61,64 @@ export const component: Registry = [
       },
     ],
   },
- 
+  {
+    name: "base-dialog",
+    type: "registry:component",
+    dependencies: ["lucide-react", "framer-motion"],
+    registryDependencies: ["dialog"],
+    files: [
+      {
+        path: "components/code-clamp/dialog/base-dialog.tsx",
+        type: "registry:component",
+      },
+    ],
+  },
+  {
+    name: "fade-dialog",
+    type: "registry:component",
+    dependencies: ["lucide-react", "framer-motion"],
+    registryDependencies: ["dialog"],
+    files: [
+      {
+        path: "components/code-clamp/dialog/fade-dialog.tsx",
+        type: "registry:component",
+      },
+    ],
+  },
+  {
+    name: "flip-dialog",
+    type: "registry:component",
+    dependencies: ["lucide-react", "framer-motion"],
+    registryDependencies: ["dialog"],
+    files: [
+      {
+        path: "components/code-clamp/dialog/flip-dialog.tsx",
+        type: "registry:component",
+      },
+    ],
+  },
+  {
+    name: "scale-dialog",
+    type: "registry:component",
+    dependencies: ["lucide-react", "framer-motion"],
+    registryDependencies: ["dialog"],
+    files: [
+      {
+        path: "components/code-clamp/dialog/scale-dialog.tsx",
+        type: "registry:component",
+      },
+    ],
+  },
+  {
+    name: "slide-dialog",
+    type: "registry:component",
+    dependencies: ["lucide-react", "framer-motion"],
+    registryDependencies: ["dialog"],
+    files: [
+      {
+        path: "components/code-clamp/dialog/slide-dialog.tsx",
+        type: "registry:component",
+      },
+    ],
+  },
 ];
